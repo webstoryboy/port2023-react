0535fb70da0119c95f41ee02ca2c45e894ea8497
webstoryboy <webstupids@gmail.com>
2023-08-04 19:35:28 +0900

    😄 리액트 헤더완료

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import React, { useState } from "react";
+
+const headerNav = [
+    {
+        title: "intro",
+        url: "#intro"
+    },
+    {
+        title: "skill",
+        url: "#skill"
+    },
+    {
+        title: "site",
+        url: "#site"
+    },
+    {
+        title: "portfolio",
+        url: "#port"
+    },
+    {
+        title: "contact",
+        url: "#contact"
+    }
+];
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    const [show, setShow] = useState(false);
+
+    const toggleMenu = () => {
+        setShow((prevShow) => !prevShow);
+    }
+
+    return (
+        <header id="header" role="banner">
+            <div className="header__inner">
+                <div className="header__logo">
+                    <h1>
+                        <a href="/">portfolio<em>react.js</em></a>
+                    </h1>
+                </div>
+                <nav 
+                    className={`header__nav ${show ? "show" : ""}`} 
+                    role="navigation" 
+                    aria-label="메인메뉴"
+                >
+                    <ul>
+                        {headerNav.map((nav, key) => (
+                            <li key={key}>
+                                <a href={nav.url}>{nav.title}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+                <div 
+                    className="header__nav__mobile" 
+                    id="headerToggle" 
+                    aria-controls="primary-menu" 
+                    aria-expanded={show ? "true" : "false"} 
+                    role="button"
+                    tabIndex="0"
+                    onClick={toggleMenu}
+                >
+                    <span></span>
+                </div>
+            </div>
+        </header>
+    )
 }
 
-export default Header
+export default Header;
