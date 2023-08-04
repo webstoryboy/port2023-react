6fe91a2df00236d7c1be3c5c8c6e5f09e6a79043
webstoryboy <webstupids@gmail.com>
2023-08-05 07:38:08 +0900

    😂 인트로 완료

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
 
-const Intro = () => {
-  return (
-    <div>Intro</div>
-  )
+import about from "../assets/img/about.jpg";
+
+const introText = {
+    title: "port developer",
+    desc: ["talent is", "found at the end of the", "effort"]
 }
 
-export default Intro
+const Intro = () => {
+    return (
+        <section id="intro">
+            <div className="intro__inner">
+                <h2 className="intro__title">
+                    {introText.title}
+                </h2>
+                <div className="intro__lines" aria-hidden="true">
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                </div>
+                <div className="intro__text">
+                    <div className="text">
+                        <div>{introText.desc[0]}</div>
+                        <div>{introText.desc[1]}</div>
+                        <div>{introText.desc[2]}</div>
+                    </div>
+                    <div className="img">
+                        <img src={about} alt="어바웃미" />
+                    </div>
+                </div>
+                <div className="intro__lines bottom" aria-hidden="true">
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Intro;
