ca43868b79fab01c9c88c44268da6d2c0e22d814
webstoryboy <webstupids@gmail.com>
2023-08-05 10:09:46 +0900

    네트리파이 에러 수정

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Header from "../components/Header";
 import Skip from "../components/Skip";
 import Intro from "../components/Intro";
